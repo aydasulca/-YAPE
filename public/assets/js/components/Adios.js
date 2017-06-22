4a894d3c912d4e5c3743b4007c1db7808cacab95
Ayda Sulca <suni_yuri_94@hotmail.com>
2017-06-21 21:07:20 -0500

    input-valor

--- public/assets/js/components/Adios.js
+++ public/assets/js/components/Adios.js
@@ -1,27 +1,46 @@
 const Se= (update) => {
   const form = $('<form action="#"></form>');
   const input = $('<input id="hola" type="number" name="" value="">');
-  const button = $('<button class="waves-effect waves-light btn" type="button" name="button">Registrarme</button>');
+  //const button = $('<button class="waves-effect waves-light btn" type="button" name="button">Registrarme</button>');
 
   form.append(input);
-  form.append(button);
+  //form.append(button);
+  //console.log(input.val());
+  //console.log(state.phone.code);
 
-  button.on('click',(e) => {
-    e.preventDefault();
-    
-    $.post('api/resendCode/', {
-      phone:state.phone.phone,
-    }, (respons) => {
-      console.log(respons);
-      console.log(input.val());
-      console.log(state.phone.code);
+  /*
+  $.post('api/resendCode/', {
+    phone:state.phone.phone,
+  }, (respons) => {
+    // console.log(respons.data);
+    if (respons.success == true) {
 
-      if (input.val() == state.phone.code) {
-        state.screen = "hola";
-        update();
-      }
+    }
+    setInterval(function(){ console.log(respons.data.code.gen()); }, 3000);
 
-    }, 'json')
+  }, 'json')
+
+  */
+  $( input).keyup(function() {
+    //alert( "Handler for .keyup() called." );
+    if (input.val() == state.phone.code) {
+      state.screen = "hola";
+      update();
+    }
+  });
+  /*
+$(input).change(function() {
+  console.log(input.val());
 })
+*/
+/*
+$( input ).keypress(function() {
+  var value = $( this ).val();
+  console.log(value);
+//  $( p ).text( value );
+})
+.keyup();
+*/
+
 return form;
 }
--- public/assets/js/components/hola1.js
+++ public/assets/js/components/hola1.js
@@ -1,2 +1,4 @@
 const Suma= (update) => {
+  const button = $('<button class="waves-effect waves-light btn" type="button" name="button">Registrarme</button>');
+return button;
 }
